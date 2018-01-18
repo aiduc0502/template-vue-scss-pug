d20a0e3037d023a11692b9f75b3cd4c12e4f764d
Duc Vuong <batnapwantai_kissemlancuoi1@yahoo.com>
2018-01-18 15:32:33 +0700

    added sass

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,21 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
+    img(src="./assets/images/logo.png")
     h1 {{msg}}
-    h2 Test
-    ul
-      li
-        a(href="https://vuejs.org" target="_blank") Vuejs
-      li
-        a(href="https://vuejs.org" target="_blank") Angularjs
-      li
-        a(href="https://vuejs.org" target="_blank") Reactjs
-      li
-        a(href="https://vuejs.org" target="_blank") React Native  
+    <test></test>
 </template>
 
-<script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+<script >
+  import Test from './components/test.vue';
+  export default {
+    name: 'app',
+    components: {
+      'test': Test
+    },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
     }
   }
-}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  h1 {
-    color: red;
-  }
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
